ee9b9a939a7f7ad1f7d988472adf1bfb153dae80
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-07 17:32:48 -0500

    Add logic to SmallStraight

--- src/Rules.js
+++ src/Rules.js
@@ -66,8 +66,19 @@ class FullHouse extends Rule {
 
 /** Check for small straights. */
 
-class SmallStraight {
-  // TODO
+class SmallStraight extends Rule {
+  evalRoll = (dice) => {
+    const d = new Set(dice);
+    console.log(d);
+    // small straight must be 4 different dice & 1,2,3,4 OR 2,3,4,5 OR 3,4,5,6
+    if (d.has(2) && d.has(3) && d.has(4)) {
+      if (d.has(1) || d.has(5)) return this.score;
+    }
+    if (d.has(3) && d.has(4) && d.has(5)) {
+      if (d.has(2) || d.has(6)) return this.score;
+    }
+    return 0;
+  };
 }
 
 /** Check for large straights. */
