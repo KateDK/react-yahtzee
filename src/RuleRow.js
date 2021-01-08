8cd2de7249a6a8e1a48856db4cd0a3daf6182937
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-08 16:16:58 -0500

    Refactor for rule line overuse

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import './RuleRow.css';
 
 class RuleRow extends Component {
-  handleClick = () => {
-    if (this.props.score === undefined) {
-      this.props.doScore();
-    }
-  };
   render() {
     const ruleClass =
       this.props.score === undefined
         ? 'RuleRow RuleRow-active'
         : 'RuleRow RuleRow-disabled';
     return (
-      <tr className={ruleClass} onClick={this.handleClick}>
+      <tr
+        className={ruleClass}
+        onClick={this.props.score === undefined ? this.props.doScore : null}
+      >
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
