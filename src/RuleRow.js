1be4456d5a9e59fb8082abcb2c4f436e0d48f570
Kate Dubitski <kate.dubitski@gmail.com>
2021-01-08 16:10:15 -0500

    Add method to prevent using rule twice

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './RuleRow.css'
 
 class RuleRow extends Component {
+  handleClick = () => {
+    if (this.props.score === undefined) {
+      this.props.doScore();
+    }
+  };
   render() {
     return (
       <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
